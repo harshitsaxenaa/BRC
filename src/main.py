893b767ac60b7db3996f83fd598cec5a0ff6288c
harshitsaxenaa <harshitsaxena0803@gmail.com>
2025-03-22 02:12:21 +0530

    Optimized main.py for performance

--- src/main.py
+++ src/main.py
@@ -1,54 +1,53 @@
 from math import ceil
-from collections import defaultdict
+import os
 
 def round1(x):
     return ceil(x * 10) / 10
 
+def process_line(line):
+    sep = line.find(b";")
+    if sep == -1:
+        return None, None
+    city = line[:sep]
+    try:
+        score = float(line[sep + 1:])
+        return city, score
+    except ValueError:
+        return None, None
+
 def main():
-    import os
+    city_stats = {}
+    buffer_size = 8192  # Read in chunks of 8KB
 
-    read = os.read
-    write = os.write
-    path_in = "testcase.txt"
-    path_out = "output.txt"
-    size = os.path.getsize(path_in)
+    fd_in = os.open("testcase.txt", os.O_RDONLY)
+    while True:
+        data = os.read(fd_in, buffer_size)
+        if not data:
+            break
+        lines = data.splitlines()
+        for line in lines:
+            city, score = process_line(line)
+            if city is None or score is None:
+                continue
 
-    
-    stats = defaultdict(lambda: [float('inf'), 0.0, float('-inf'), 0])
+            if city in city_stats:
+                stat = city_stats[city]
+                if score < stat[0]: stat[0] = score
+                stat[1] += score
+                if score > stat[2]: stat[2] = score
+                stat[3] += 1
+            else:
+                city_stats[city] = [score, score, score, 1]
 
-    fd_in = os.open(path_in, os.O_RDONLY)
-    raw = read(fd_in, size)
-    os.close(fd_in)
-
-    lines = raw.split(b"\n")
-
-    for line in lines:
-        if not line:
-            continue
-        sep = line.find(b";")
-        if sep == -1:
-            continue
-        city = line[:sep]
-        try:
-            score = float(line[sep + 1:])
-        except:
-            continue
-        stat = stats[city]
-        if score < stat[0]: stat[0] = score
-        stat[1] += score
-        if score > stat[2]: stat[2] = score
-        stat[3] += 1
-
-  
-    output = bytearray()
-    for city in sorted(stats):
-        mn, sm, mx, cnt = stats[city]
+    out = bytearray()
+    for city in sorted(city_stats):
+        mn, sm, mx, cnt = city_stats[city]
         mean = sm / cnt
-        output += city + b"=" + \
-            f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode()
+        out += city + b"=" + \
+               f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode()
 
-    fd_out = os.open(path_out, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
-    write(fd_out, output)
+    fd_out = os.open("output.txt", os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    os.write(fd_out, out)
     os.close(fd_out)
 
 if __name__ == "__main__":
