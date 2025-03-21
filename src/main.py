7dc1e2f6d4407c8ce6290b2d2abbef57296647ca
harshitsaxenaa <harshitsaxena0803@gmail.com>
2025-03-22 02:05:31 +0530

    Optimized main.py for performance

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from math import ceil
+from collections import defaultdict
 
 def round1(x):
     return ceil(x * 10) / 10
@@ -6,41 +7,48 @@ def round1(x):
 def main():
     import os
 
-    city_stats = {}
     read = os.read
     write = os.write
-    fd_in = os.open("testcase.txt", os.O_RDONLY)
-    data = read(fd_in, os.path.getsize("testcase.txt")).splitlines()
+    path_in = "testcase.txt"
+    path_out = "output.txt"
+    size = os.path.getsize(path_in)
+
+    
+    stats = defaultdict(lambda: [float('inf'), 0.0, float('-inf'), 0])
+
+    fd_in = os.open(path_in, os.O_RDONLY)
+    raw = read(fd_in, size)
     os.close(fd_in)
 
-    for line in data:
+    lines = raw.split(b"\n")
+
+    for line in lines:
+        if not line:
+            continue
         sep = line.find(b";")
         if sep == -1:
             continue
         city = line[:sep]
         try:
-            score = float(line[sep+1:])
+            score = float(line[sep + 1:])
         except:
             continue
+        stat = stats[city]
+        if score < stat[0]: stat[0] = score
+        stat[1] += score
+        if score > stat[2]: stat[2] = score
+        stat[3] += 1
 
-        if city in city_stats:
-            stat = city_stats[city]
-            if score < stat[0]: stat[0] = score
-            stat[1] += score
-            if score > stat[2]: stat[2] = score
-            stat[3] += 1
-        else:
-            city_stats[city] = [score, score, score, 1]
-
-    out = bytearray()
-    for city in sorted(city_stats):
-        mn, sm, mx, cnt = city_stats[city]
+  
+    output = bytearray()
+    for city in sorted(stats):
+        mn, sm, mx, cnt = stats[city]
         mean = sm / cnt
-        out += city + b"=" + \
-               f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode()
+        output += city + b"=" + \
+            f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode()
 
-    fd_out = os.open("output.txt", os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
-    write(fd_out, out)
+    fd_out = os.open(path_out, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    write(fd_out, output)
     os.close(fd_out)
 
 if __name__ == "__main__":
