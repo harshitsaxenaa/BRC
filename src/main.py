338dd02d6b84a9cb06abbc7d426f15648192d6aa
harshitsaxenaa <harshitsaxena0803@gmail.com>
2025-03-22 02:28:10 +0530

    Optimized main.py for performance

--- src/main.py
+++ src/main.py
@@ -7,40 +7,49 @@ def round1(x):
 def main():
     import os
 
+    read = os.read
+    write = os.write
     path_in = "testcase.txt"
     path_out = "output.txt"
+    size = os.path.getsize(path_in)
 
+    
     stats = defaultdict(lambda: [float('inf'), 0.0, float('-inf'), 0])
 
-   
-    with open(path_in, "rb") as fd_in:
-        for line in fd_in:
-            if not line:
-                continue
-            sep = line.find(b";")
-            if sep == -1:
-                continue
-            city = line[:sep]
-            try:
-                score = float(line[sep + 1:])
-            except ValueError:
-                continue
-            stat = stats[city]
-            if score < stat[0]: stat[0] = score
-            stat[1] += score
-            if score > stat[2]: stat[2] = score
-            stat[3] += 1
+    fd_in = os.open(path_in, os.O_RDONLY)
+    raw = read(fd_in, size)
+    os.close(fd_in)
 
-    
-    output = []
-    for city in sorted(stats.keys()):
+    lines = raw.split(b"\n")
+
+    for line in lines:
+        if not line:
+            continue
+        sep = line.find(b";")
+        if sep == -1:
+            continue
+        city = line[:sep]
+        try:
+            score = float(line[sep + 1:])
+        except:
+            continue
+        stat = stats[city]
+        if score < stat[0]: stat[0] = score
+        stat[1] += score
+        if score > stat[2]: stat[2] = score
+        stat[3] += 1
+
+  
+    output = bytearray()
+    for city in sorted(stats):
         mn, sm, mx, cnt = stats[city]
         mean = sm / cnt
-        output.append(city + b"=" + f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode())
+        output += city + b"=" + \
+            f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode()
 
-    
-    with open(path_out, "wb") as fd_out:
-        fd_out.writelines(output)
+    fd_out = os.open(path_out, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    write(fd_out, output)
+    os.close(fd_out)
 
 if __name__ == "__main__":
     main()
