bbd6dc03d1473c72b670e953178e18ed34d1d1c2
harshitsaxenaa <harshitsaxena0803@gmail.com>
2025-03-22 04:33:08 +0530

    Optimized main.py for performance

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 from math import ceil
-import os, mmap
+import os
 
 def round1(x):
     return ceil(x * 10) / 10
 
 def main():
-    input_path = "testcase.txt"
-    output_path = "output.txt"
-
-    with open(input_path, "rb") as f:
-        mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-        lines = mm.read().split(b"\n")
-
     city_stats = {}
+    read = os.read
+    write = os.write
 
-    for line in lines:
+    fd_in = os.open("testcase.txt", os.O_RDONLY)
+    data = read(fd_in, os.path.getsize("testcase.txt")).splitlines()
+    os.close(fd_in)
+
+    get = city_stats.get
+    for line in data:
         sep = line.find(b";")
         if sep == -1:
             continue
@@ -24,8 +24,8 @@ def main():
         except:
             continue
 
-        if city in city_stats:
-            stat = city_stats[city]
+        stat = get(city)
+        if stat:
             if score < stat[0]: stat[0] = score
             stat[1] += score
             if score > stat[2]: stat[2] = score
@@ -34,15 +34,23 @@ def main():
             city_stats[city] = [score, score, score, 1]
 
     out = bytearray()
-    join = b"=".join
+    append = out.extend  # faster access
+
     for city in sorted(city_stats):
         mn, sm, mx, cnt = city_stats[city]
         mean = sm / cnt
-        out += city + b"=" + \
-               f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode()
+        append(city)
+        append(b"=")
+        append(f"{round1(mn):.1f}".encode())
+        append(b"/")
+        append(f"{round1(mean):.1f}".encode())
+        append(b"/")
+        append(f"{round1(mx):.1f}".encode())
+        append(b"\n")
 
-    with open(output_path, "wb") as f:
-        f.write(out)
+    fd_out = os.open("output.txt", os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    write(fd_out, out)
+    os.close(fd_out)
 
 if __name__ == "__main__":
     main()
