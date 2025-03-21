1ef7b9d455b7815f7586ce68641dd18d57d689a5
harshitsaxenaa <harshitsaxena0803@gmail.com>
2025-03-22 03:10:32 +0530

    Optimized main.py for performance

--- src/main.py
+++ src/main.py
@@ -1,23 +1,17 @@
 from math import ceil
-from collections import defaultdict
 
-def round1(x):
+def round_up(x):
     return ceil(x * 10) / 10
 
 def main():
-    import os
+    input_file = "testcase.txt"
+    output_file = "output.txt"
 
-    path_in = "testcase.txt"
-    path_out = "output.txt"
+    city_stats = {}
 
-    stats = defaultdict(lambda: [float('inf'), 0.0, float('-inf'), 0])
-
-    # Read and process the file line-by-line
-    with open(path_in, "rb") as fd_in:
-        for line in fd_in:
-            if not line:
-                continue
-            sep = line.find(b";")
+    with open(input_file, "r") as f:
+        for line in f:
+            sep = line.find(";")
             if sep == -1:
                 continue
             city = line[:sep]
@@ -25,22 +19,21 @@ def main():
                 score = float(line[sep + 1:])
             except ValueError:
                 continue
-            stat = stats[city]
-            if score < stat[0]: stat[0] = score
-            stat[1] += score
-            if score > stat[2]: stat[2] = score
-            stat[3] += 1
 
-    # Prepare output efficiently
-    output = []
-    for city in sorted(stats.keys()):
-        mn, sm, mx, cnt = stats[city]
-        mean = sm / cnt
-        output.append(city + b"=" + f"{round1(mn):.1f}/{round1(mean):.1f}/{round1(mx):.1f}\n".encode())
+            if city in city_stats:
+                stats = city_stats[city]
+                if score < stats[0]: stats[0] = score     # min
+                stats[1] += score                          # sum
+                if score > stats[2]: stats[2] = score     # max
+                stats[3] += 1
+            else:
+                city_stats[city] = [score, score, score, 1]
 
-    # Write all at once to reduce I/O overhead
-    with open(path_out, "wb") as fd_out:
-        fd_out.writelines(output)
+    with open(output_file, "w") as f:
+        for city in sorted(city_stats):
+            mn, sm, mx, cnt = city_stats[city]
+            mean = sm / cnt
+            f.write(f"{city}={round_up(mn):.1f}/{round_up(mean):.1f}/{round_up(mx):.1f}\n")
 
 if __name__ == "__main__":
     main()
